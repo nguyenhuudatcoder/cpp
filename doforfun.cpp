58989b62854bb0f42c1c19889e085567074817a6
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2024-11-22 08:43:10 +0700

    first commit

---
+++ doforfun.cpp
@@ -0,0 +1,15 @@
+///Kiên is reliable!!!
+///Made by Nguyễn Hữu Đạt
+#include<bits/stdc++.h>
+//#define int long long
+#define kien main
+#define mod 1000000007
+using namespace std;
+
+kien()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+}
