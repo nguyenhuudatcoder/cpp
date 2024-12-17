7a6d4c6daae9eb236f1bea2ed2f464462f4932fc
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2024-12-17 14:55:42 +0700

    pushing

--- School/Grade9/PHATQUA-27-11-2024/SPLIT.cpp
+++ School/Grade9/PHATQUA-27-11-2024/SPLIT.cpp
@@ -1,12 +1,15 @@
-///A game is our childhood
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// A game is our childhood
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n, m;
+string a, b;
+int k;
+int sub[1005][1005];
 kien()
 {
     // if(fopen(".inp","r"))
@@ -17,5 +20,17 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    cin >> n >> m >> k;
+    cin >> a >> b;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+                sub[i][j] = sub[i - 1][j - 1] + 1;
+            sub[i][j] = max({sub[i][j - 1], sub[i][j], sub[i - 1][j]});
+        }
+    if (sub[n][m] >= k)
+        cout << sub[n][m];
+    else
+        cout << -1;
 }
