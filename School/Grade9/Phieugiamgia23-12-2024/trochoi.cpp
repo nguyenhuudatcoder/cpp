510fd0cfa55816980c2780826c641a07188b70bb
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2024-12-23 16:40:40 +0700

    incomplete

--- School/Grade9/Phieugiamgia23-12-2024/trochoi.cpp
+++ School/Grade9/Phieugiamgia23-12-2024/trochoi.cpp
@@ -1,12 +1,29 @@
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-int n,a[1000005],b[1000005];
-ll ans=inf;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+ll n, a[1000005], b[1000005];
+ll ans = inf;
+void sub1()
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+
+            ans = min(ans, abs(a[i] + b[j]));
+}
+void sub2()
+{
+    // sort(a, a + n);
+    sort(b, b + n);
+    for (ll i = 0; i < n; i++)
+    {
+        ll x = *lower_bound(b, b + n, -a[i]);
+        ans = min(ans, abs(a[i] + x));
+    }
+}
 kien()
 {
     // if(fopen(".inp","r"))
@@ -17,15 +34,14 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
-    for(int i=0;i<n;i++) cin>>b[i];
-    sort(a,a+n);
-    sort(b,b+n,greater<int>());
-    for(int i=0;i<n;i++)
-    {
-        int x=lower_bound(b,b+n,-x)-b;
-        ans=min(ans,1ll*abs(a[i]+b[x]));
-    }
-    cout<<ans;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+        cin >> a[i];
+    for (ll i = 0; i < n; i++)
+        cin >> b[i];
+    if (n <= 9 * 1e3)
+        sub1();
+    else
+        sub2();
+    cout<< ans;
 }
