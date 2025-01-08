18332839e6a79960f4a9c0bf18245f7d57bea59a
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-08 21:56:58 +0700

    Accepted

--- School/Grade9/8-1-2025-2/bai5.cpp
+++ School/Grade9/8-1-2025-2/bai5.cpp
@@ -5,18 +5,7 @@
 using namespace std;
 const ll inf = LLONG_MAX;
 const ll mod = 1e9 + 7;
-int t, n;
-struct shit
-{
-    int park, leave;
-};
-shit a[10005];
-bool compare(shit a, shit b)
-{
-    if (a.park == b.park)
-        return a.leave < b.leave;
-    return a.park < b.park;
-}
+int t, n, a[10005], b[10005], s[2405];
 kien()
 {
     // if(fopen(".inp","r"))
@@ -26,19 +15,25 @@ kien()
     // }
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
+    cout.tie(0);cin>>t;
     while (t--)
     {
         cin >> n;
         for (int i = 0; i < n; i++)
-        {
-            cin >> a[i].park;
-            // a[i].park = a[i].park % 100 * 60 + a[i].park % 100;
-        }
+            cin >> a[i];
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i].leave;
-            // a[i].leave = a[i].leave % 100 * 60 + a[i].leave % 100;
+            cin >> b[i];
+            s[a[i]]++;
+            if(b[i]>=a[i])
+            s[b[i]]--;
         }
+        int res = 0, ans = 0;
+        for (int i = 0; i <= 2400; i++)
+        {
+            res += s[i];
+            ans = max(ans, res);
+            s[i] = 0;
+        }cout<<ans<<"\n";
     }
 }
