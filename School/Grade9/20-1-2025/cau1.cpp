4b6d495a639c6ef1c09759ca9403acbcd40d642f
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-20 14:20:53 +0700

    Accepted

---
+++ School/Grade9/20-1-2025/cau1.cpp
@@ -0,0 +1,26 @@
+///Made by Nguyễn Hữu Đạt
+#include<bits/stdc++.h>
+#define ll long long
+#define kien main
+using namespace std;
+const ll inf=LLONG_MAX;
+const ll mod=1e9+7;
+ll n;
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>n;
+    ll ans=0;
+    while(n>0)
+    {
+        ans+=n/5;
+        n/=5;
+    }cout<<ans;
+}
