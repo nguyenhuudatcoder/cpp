e1cb0787351f686376de32c2436a4feb9a75399b
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-21 19:02:48 +0700

    Declined

---
+++ School/Grade9/21-1-2025/cau5.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int k,n;
+int a[100005];
+bool Monica(int m)
+{
+    bool
+}
+void ChatGPT()
+{
+    int l=0,r=*max_element(a,a+n);
+    while(l<=r)
+    {
+        int m=1ll*(l+r)/2;
+
+    }
+}
+main()
+{
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+}
