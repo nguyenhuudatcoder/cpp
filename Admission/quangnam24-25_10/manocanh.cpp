47575f89b621ec65702666963ae2bb6841b688ac
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-02-22 00:09:38 +0700

    Declined

---
+++ Admission/quangnam24-25_10/manocanh.cpp
@@ -0,0 +1,23 @@
+///Made by Nguyễn Hữu Đạt
+#include<bits/stdc++.h>
+#define ll long long
+#define kien main
+using namespace std;
+const ll inf=LLONG_MAX;
+const ll mod=1e9+7;
+int m,n;
+ll v[1005][1005];
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>m>>n;
+    for(int i=0;i<m;i++)for(int j=0;j<n;j++)cin>>v[i][j];
+    
+}
